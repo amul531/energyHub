5146d7f3f1792beb07a6a94ea737d35e0af30088
amul531 <amul531@gmail.com>
2016-08-01 15:41:32 -0400

    Refactored code
    Implemented some java8 features (forEach, reduce methods)

--- BashCartesian.java
+++ BashCartesian.java
@@ -1,11 +1,12 @@
 /*
 Clone or download this program and run it through command line or any java editor of your choice.
 Uncomment any testing block of choice in the main function before compiling and running it.
-*/
+ */
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class BashCartesian{
 	//main
@@ -22,124 +23,12 @@ public class BashCartesian{
 			else
 				print(bashCartesianProduct(inputStr));
 		}
-		*/
-
-		//Uncomment and run this to check sample inputs and outputs (block test)
-		/*
-	    inputStr = new String("afe{g,h{k,l{o,p}q}m}ij{x,y}"); 
+		 */		
+		/*inputStr = new String("a{b,c{d,e}f,g}hi{j,k}"); 
 		System.out.println("Input #1: "+inputStr);
 		print(bashCartesianProduct(inputStr));
-		//expected output: afegijx afegijy afehkmijx afehkmijy afehloqmijx afehloqmijy afehlpqmijx afehlpqmijy 
-		
-		inputStr = new String("a{b,c}d{e,f,g}hi"); 
-		System.out.println("\n\nInput #2: "+inputStr);
-		print(bashCartesianProduct(inputStr));
-		//expected output: abdehi abdfhi abdghi acdehi acdfhi acdghi
-		
-		inputStr = new String("a{b,c{d,e,f}g,h}ij{k,l}"); 
-		System.out.println("\n\nInput #2: "+inputStr);
-		print(bashCartesianProduct(inputStr));
-		//expected output: abijk abijl acdgijk acdgijl acegijk acegijl acfgijk acfgijl ahijk ahijl
-
-		inputStr = new String("afe{{k,l{o,p}q}m}ij"); 
-		System.out.println("\n\nInput #2: "+inputStr);
-		print(bashCartesianProduct(inputStr));
-		//expected output: afekmij afeloqmij afelpqmij 
-
-		inputStr = new String("ab{c,d}e{f,g}");  
-		System.out.println("\n\nInput #3: "+inputStr);
-		print(bashCartesianProduct(inputStr));
-		//expected output: abcef abceg abdef abdeg 
-
-		inputStr = new String("{a,b}d{e,f{g,h}}ij");
-		System.out.println("\n\nInput #4: "+inputStr);
-		print(bashCartesianProduct(inputStr));
-		//expected output: adeij adfgij adfhij bdeij bdfgij bdfhij 
+		//expected output:abhij,abhik,acdfhij,acdfhik,acefhij,acefhik,aghij,aghik
 		*/
-		
-		//Uncomment and run this to test it with a set of inputs (unit tests)
-		/*
-		String[] inputArr;
-		String[] outputArr;
-		boolean flag = false;
-
-		//test#1 :non-nested string
-		inputStr = "a{b,c}d{e,f}g";
-		System.out.println("Test#1 :"+inputStr);	
-		inputArr = bashCartesianProduct(inputStr).split(",");
-		outputArr = "abdeg,abdfg,acdeg,acdfg".split(",");
-		for(int i=0;i<inputArr.length;i++)
-			if(inputArr[i].equals(outputArr[i]));
-		flag = true;
-		System.out.println(flag);
-
-		//test#2 :non-nested string
-		inputStr = "{a,b}{c,d}";
-		System.out.println("Test#2 :"+inputStr);	
-		inputArr = bashCartesianProduct(inputStr).split(",");
-		outputArr = "ac,ad,bc,bd".split(",");
-		flag = false;
-		for(int i=0;i<inputArr.length;i++)
-			if(inputArr[i].equals(outputArr[i]));
-		flag = true;
-		System.out.println(flag);
-
-		//test#3 :nested string
-		inputStr = "a{b,c{d,e}f,g}hi{j,k}";
-		System.out.println("Test#3 :"+inputStr);	
-		inputArr = bashCartesianProduct(inputStr).split(",");
-		outputArr = "aabhij,abhik,acdfhij,acdfhik,acefhij,acefhik,aghij,aghik".split(",");
-		flag = false;
-		for(int i=0;i<inputArr.length;i++)
-			if(inputArr[i].equals(outputArr[i]));
-		flag = true;
-		System.out.println(flag);
-
-		//test#4 :multiple-nested string
-		inputStr = "{{{{a,b},c}d,e}f,g}";
-		System.out.println("Test#4 :"+inputStr);	
-		inputArr = bashCartesianProduct(inputStr).split(",");
-		outputArr = "acd,bcd,cdf,ef,g".split(",");
-		flag = false;
-		for(int i=0;i<inputArr.length;i++)
-			if(inputArr[i].equals(outputArr[i]));
-		flag = true;
-		System.out.println(flag);
-		
-		//test#5 :given example #1
-		inputStr = "a{b,c}d{e,f,g}hi";
-		System.out.println("Test#5 :"+inputStr);	
-		inputArr = bashCartesianProduct(inputStr).split(",");
-		outputArr = "abdehi abdfhi abdghi acdehi acdfhi acdghi".split(" ");
-		flag = false;
-		for(int i=0;i<inputArr.length;i++)
-			if(inputArr[i].equals(outputArr[i]));
-		flag = true;
-		System.out.println(flag);
-		
-		//test#6 :given example #2
-		inputStr = "a{b,c{d,e,f}g,h}ij{k,l}";
-		System.out.println("Test#6 :"+inputStr);	
-		inputArr = bashCartesianProduct(inputStr).split(",");
-		outputArr = "abijk abijl acdgijk acdgijl acegijk acegijl acfgijk acfgijl ahijk ahijl".split(" ");
-		flag = false;
-		for(int i=0;i<inputArr.length;i++)
-			if(inputArr[i].equals(outputArr[i]));
-		flag = true;
-		System.out.println(flag);
-
-		//test#7 :improperly balanced braces 
-		//throws error
-		boolean thrown = false;
-		try {
-			inputStr = "{{{{a,b},c}d,ef,g}";
-			System.out.println("Test#7 :"+inputStr);	
-			inputArr = bashCartesianProduct(inputStr).split(",");
-		} catch (IllegalArgumentException e) {
-			thrown = true;
-		}
-		System.out.println(thrown); 
-		*/		
 	}
 
 	protected static String bashCartesianProduct(String input){
@@ -160,18 +49,17 @@ public class BashCartesian{
 
 	private static ArrayList<int[]> nestIndexList(String input){
 		ArrayList<int[]> indices = new ArrayList<int[]>();                                 //an arraylist to store the start and end position of each nested bracket
-		int open = 0, start = 0, end = 0;
-		int[] addIndex = new int[2];                                                       //list to store the start and end indices of a nest
+		int open = 0, start = 0, end = 0;                                                      //list to store the start and end indices of a nest
 		boolean nested = false;                                                            //true if a nest is encountered
 		for(int i = 0; i < input.length(); i++){			
 			if(input.charAt(i) == '{'){                                                //check for nested open bracket
 				open++;
 				if(!nested && open == 2)
 					start = i;
-			}
+			}			
 			if(input.charAt(i) == '}'){                                                //find the closing bracket that matches the above open bracket
 				open--;     
-				if(!nested && open == 1){
+				if(!nested && open==1){
 					end = i;
 					nested = true;                                             //change nested to true since a nest has been found
 				}
@@ -181,75 +69,116 @@ public class BashCartesian{
 					start--;
 				boolean closed = false;                                            //true if close bracket found else false
 				for(int j = end; j < input.length()-1; j++){                       //checking for further nests within the current nest
-					if(input.charAt(j) == '}')
-						closed = true;					
-					if(closed && (Character.isLetter(input.charAt(j)) || input.charAt(j) == '}') && (input.charAt(j+1) == ',' || input.charAt(j+1) == '}')){
+					if(input.charAt(j) == '}'){
+						closed = true;
+					}
+					if(closed && (input.charAt(j+1) == ',' || input.charAt(j+1) == '}')){
 						end = j;
 						break;
 					}
 				}
-				addIndex[0] = start;
-				addIndex[1] = end;
-				indices.add(addIndex);
-				nested = false;
 				i = end;
-			}                                                                          //end of if(nested)
+				updateIndex(start,end,indices);
+				nested = false;
+			}  //end of if(nested)
 		}
 		return indices;
+	}	
+	
+	//updates the arrayList which contains the indices of nested braces 
+	public static void updateIndex(int s, int e, ArrayList<int[]> indices ){             
+		int[] addIndex = new int[2]; 
+		addIndex[0] = s;
+		addIndex[1] = e;
+		indices.add(addIndex);
 	}
 
 	private static String finalProduct(String input){
-		StringBuilder output = new StringBuilder();
-		int open = 0;                                                                      //to keep track of # of open(++) and close(--) braces. 0 indicates well balanced braces
-		int count = 0;                                                                     //total # of open braces encountered
-		for(int i = 0; i < input.length(); i++){                                           //check to see if braces are well balanced
-			if(input.charAt(i) == '{'){
-				open++;
-				count++; 
-			}
-			if(input.charAt(i) == '}')
-				open--; 
-		}
-		if(open != 0)                                                              	   //error thrown if braces not well balanced
-			throw new IllegalArgumentException("String not well formed- " + input);
+		StringBuilder output = new StringBuilder();                                 
+		int count = checkBraces(input);                                                    //total # of open braces encountered		
+
 		int[] openBr = new int[count];                                             	   //stores the indices of open brackets
 		int[] closeBr = new int[count];                                            	   //stores the indices of close brackets
-		int[] curOptn = new int[count];                                            	   //stores the indices of the current options
-		int current = 0;
+		getBraceIndices(input, openBr, closeBr);
 
-		for(int i = 0; i < input.length(); i++){                                   	   //get the indices of all the open and close braces
-			if(input.charAt(i) == '{')
-				openBr[current] = i;
-			if(input.charAt(i) == '}')
-				closeBr[current++] = i;
-		}
+		ArrayList<String[]> braceContents = braceContents(input, openBr, closeBr);
 
-		ArrayList<String[]> inBraceAsList = new ArrayList<String[]>();              	   //all the substrings from within the braces are stored here
-		for(int i = 0; i < count; i++)
-			inBraceAsList.add((input.substring(openBr[i] + 1, closeBr[i]).split(",")));//extract the substring from within the brackets and add it to inBraceAsList
-
-		int totalCombos = 1;                                                          
-		for(int i = 0; i < inBraceAsList.size(); i++)                                	   //total number of possible combinations based on the items in inBraceAsList
-			totalCombos *= inBraceAsList.get(i).length;
-
-		for(int k = 0; k < totalCombos; k++){                                              //generate all possible outputs by iterating through all the combinations
-			StringBuilder temp = new StringBuilder(input);
-			for(int j = inBraceAsList.size() - 1; j >= 0; j--)                         //replace the braces with a string based on the current option
-				temp.replace(openBr[j], closeBr[j]+1, inBraceAsList.get(j)[curOptn[j]]);
-			output.append(temp.toString()).append(",");                                //append the value to the output list
-			                                                                       	   //updating the array containing the "current" options and returning back an updated pointer for it                                    
-			curOptn[count-1]++;                                                    	   //outer loop
-			for(int i = count - 1; i > 0; i--){
-				if(curOptn[i] == inBraceAsList.get(i).length){
-					curOptn[i] = 0;                                            //when current pointer reaches the bound, reset to 0
-					curOptn[i-1]++;                                            //inner loop 
-				}
-			}
-		}
+		int totalCombos = numOfCombinations(braceContents);                                            	   
+		output = generateAllComb(input, totalCombos, braceContents, openBr, closeBr);
 		return output.deleteCharAt(output.length()-1).toString();
 	}
 
-	public static void print(String s){                                            		   //helper function: to print a string array/list
+	//generates all possible outputs for the given combinations
+	public static StringBuilder generateAllComb(String input, int total, ArrayList<String[]> braceContents, int[] openBr, int[] closeBr){
+		StringBuilder output = new StringBuilder();  
+		int[] curItem = new int[openBr.length];                                            //stores the indices of the current options
+		
+		for(int k = 0; k < total; k++){                                                    //generate all possible outputs by iterating through all the combinations
+			StringBuilder temp = new StringBuilder(input);
+			for(int j = braceContents.size() - 1; j >= 0; j--){                         //replace the braces with a string based on the current option
+				temp.replace(openBr[j], closeBr[j]+1, braceContents.get(j)[curItem[j]]);
+			}
+			output.append(temp.toString()).append(",");                                //append the value to the output list
+			                                                                           //updating the array containing the "current" options and returning back an updated pointer for it                                    
+			curItem[openBr.length-1]++;                                                //outer loop
+			for(int i = openBr.length - 1; i > 0; i--){
+				if(curItem[i] == braceContents.get(i).length){
+					curItem[i] = 0;                                            //when current pointer reaches the bound, reset to 0
+					curItem[i-1]++;                                            //inner loop 
+				}
+			}
+		}
+		return output;
+	}
+
+	//total number of possible combinations based on the items in braceContents
+	private static int numOfCombinations(ArrayList<String[]> braceContents){
+		return braceContents.stream().mapToInt(strings -> strings.length).reduce(1, (a, b) -> a * b);
+	}
+
+	//extract the substring from within the brackets and add it to an arrayList
+	private static ArrayList<String[]> braceContents(String input,int[] openBr, int[] closeBr){
+		ArrayList<String[]> inBraceAsList = new ArrayList<String[]>();              	
+		for(int i = 0; i < openBr.length; i++)
+		{
+			inBraceAsList.add((input.substring(openBr[i] + 1, closeBr[i]).split(","))); 
+		}
+		return inBraceAsList;
+	}
+
+	private static void getBraceIndices(String input, int[] openBr, int[] closeBr){
+		int current = 0;
+		for(int i = 0; i < input.length(); i++){                                   	   
+			if(input.charAt(i) == '{'){
+				openBr[current] = i;
+			}
+			if(input.charAt(i) == '}'){
+				closeBr[current++] = i;
+			}
+		}
+	}
+
+	//this checks if the braces are well balanced and returns back the total # of pairs
+	private static int checkBraces(String input){
+		Stack<Character> braces = new Stack<Character>();                                  
+		int count =0;
+		for(int i = 0; i < input.length(); i++){                                         
+			if(input.charAt(i) == '{'){
+				braces.push(input.charAt(i));
+				count++; 
+			}
+			if(input.charAt(i) == '}')
+			{
+				braces.pop();
+			}
+		}
+		if(!braces.empty()){                                                         	   
+			throw new IllegalArgumentException("String not well formed- " + input);
+		}
+		return count;
+	}
+
+	public static void print(String s){                                            		   
 		List<String> ip = Arrays.asList(s.split(","));  
 		ip.forEach(i->System.out.print(i+" "));
 		System.out.println();
